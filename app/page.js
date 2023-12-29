e2dadd897bbe215dc03dfe77f5cdb8e05df4a95e
KarlStevenRequinto <lerdrobott@gmail.com>
2023-12-30 01:22:41 +0800

    testing dev branch

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from './styles.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      DOTA DRAFT APP
+      TESTING DEV BRANCH
     </main>
   );
 }
