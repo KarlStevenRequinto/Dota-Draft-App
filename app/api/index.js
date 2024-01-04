64fc39ad281a32ed4d9805886f14800c3398e773
KarlStevenRequinto <lerdrobott@gmail.com>
2024-01-05 02:21:13 +0800

    removed unused apis, added styling to image

--- app/api/index.js
+++ app/api/index.js
@@ -1,52 +1,3 @@
-const GetHero = async () => {
-  const myHeaders = new Headers()
-  myHeaders.append(
-    "Authorization",
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTdWJqZWN0IjoiZWQ1YTY4ZjEtMGNkMS00NDE0LWI1MzYtYzhlMzQ0MWZjOTQwIiwiU3RlYW1JZCI6IjE0NzM2Nzg4NSIsIm5iZiI6MTcwMjkxMzU2MywiZXhwIjoxNzM0NDQ5NTYzLCJpYXQiOjE3MDI5MTM1NjMsImlzcyI6Imh0dHBzOi8vYXBpLnN0cmF0ei5jb20ifQ.OLJA1BUiqvfHxtjhrcsLhAPC5zzaOVVVLGp2h1JJvOs"
-  )
-
-  const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  }
-
-  try {
-    const response = await fetch(
-      "https://api.stratz.com/api/v1/Hero",
-      requestOptions
-    )
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok")
-    }
-
-    const result = await response.json()
-    return result // Return the result to be used elsewhere
-  } catch (error) {
-    console.log("Error:", error)
-    throw error // Rethrow the error to be caught elsewhere if needed
-  }
-}
-
-const GetHeroImage = async () => {
-  try {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    }
-
-    const response = await fetch(
-      "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/antimage.png",
-      requestOptions
-    )
-    return response
-  } catch (error) {
-    console.log("error", error)
-    return null
-  }
-}
-
 async function GetHeroStats() {
   try {
     const requestOptions = {
@@ -66,4 +17,4 @@ async function GetHeroStats() {
     return null
   }
 }
-export { GetHero, GetHeroImage, GetHeroStats }
+export { GetHeroStats }
--- app/components/hero-undfrafted-container/index.js
+++ app/components/hero-undfrafted-container/index.js
@@ -1,24 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
-import React, { useEffect,useState } from "react"
+import React, { useEffect, useState } from "react"
 import { GetHeroStats } from "@/app/api"
+import styles from "./styles.module.css"
+
 const HeroUndraftedContainer = () => {
-  const [heroImage, setHeroImage] = useState(null)
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const heroStats = await GetHeroStats()
-        const imageUrl = `https://api.opendota.com${heroStats[0].img}`
-        setHeroImage(imageUrl)
-      } catch (error) {
-        console.error("Error fetching hero data:", error)
-      }
-    }
-
-    fetchData()
-  }, [])
-
-  return <div>{heroImage && <img src={heroImage} alt="Hero Image" />}</div>
+  const heroImage = "/static/images/antimage.png"
+  
+  return (
+    // make the container shadow later
+    <div className={styles.container}>
+      {heroImage && (
+        <img src={heroImage} className={styles.image} alt="Hero Image" />
+      )}
+    </div>
+  )
 }
 
 export default HeroUndraftedContainer
