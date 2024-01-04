60cdaa42029f072a491cc7b53055249f05ae0874
KarlStevenRequinto <lerdrobott@gmail.com>
2024-01-05 01:59:24 +0800

    utilize getHeroStats api

--- app/components/hero-undfrafted-container/index.js
+++ app/components/hero-undfrafted-container/index.js
@@ -1,41 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
-import React, { useEffect, useState } from "react"
-import { getData, getHero } from "@/app/api"
+import React, { useEffect,useState } from "react"
+import { GetHeroStats } from "@/app/api"
 const HeroUndraftedContainer = () => {
+  const [heroImage, setHeroImage] = useState(null)
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const heroData = await getHero()
-        const valuesArray = Object.values(heroData)
-        valuesArray.forEach((hero) => {
-          console.log(hero.id) // This will log each individual item
-          // You can access specific properties of each item like hero.id, hero.name, etc.
-        })
+        const heroStats = await GetHeroStats()
+        const imageUrl = `https://api.opendota.com${heroStats[0].img}`
+        setHeroImage(imageUrl)
       } catch (error) {
         console.error("Error fetching hero data:", error)
-        // Handle error
       }
     }
 
     fetchData()
   }, [])
 
-  return (
-    <div>
-      <video
-        poster="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/antimage.png"
-        autoPlay
-        preload="auto"
-        loop
-        playsInline
-      >
-        <source
-          type="video/webm"
-          src="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/antimage.webm"
-        ></source>
-      </video>
-    </div>
-  )
+  return <div>{heroImage && <img src={heroImage} alt="Hero Image" />}</div>
 }
 
 export default HeroUndraftedContainer
