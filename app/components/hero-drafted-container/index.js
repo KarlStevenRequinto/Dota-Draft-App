542917358a47808f9b2ef4ec3fca15dcb6580d09
KarlStevenRequinto <lerdrobott@gmail.com>
2024-01-19 09:10:41 +0800

    adding video bg and fix headers

--- app/components/hero-drafted-container/index.js
+++ app/components/hero-drafted-container/index.js
@@ -1,54 +1,55 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react"
-import styles from "./styles.module.css"
-import Image from "next/image"
+import React from "react";
+import styles from "./styles.module.css";
+import Image from "next/image";
 
 const HeroDraftedContainer = ({ type }) => {
-  const getTypeBackground = (type) => {
-    let background
-    if (type === "team") {
-      background = `linear-gradient(to left, var(--green-01), var(--green-02))`
-    } else if (type === "enemy") {
-      background = `linear-gradient(to right, var(--red-01), var(--red-02))`
-    } else if (type === "ban") {
-      background =
-        "`linear-gradient(to right, var(--neutral-01), var(--neutral-01))`"
-    }
-    return background
-  }
-  return (
-    // main container
-    <div
-      className={`${styles.container} ${
-        type === "team" ? styles.team : type === "enemy" ? styles.enemy : ""
-      }`}
-    >
-      {/* shadow container */}
-      <div className={styles.outerShadowContainer}>
-        {/* image container */}
-        <div className={styles.imageContainer}>
-          <div className={styles.imageWrapper}>
-            <Image
-              // src should be a prop
-              src="/static/images/antimage.png"
-              alt="where's the hero image bro?"
-              width={80}
-              height={65}
-            />
-          </div>
+    const getTypeBackground = (type) => {
+        let background;
+        if (type === "team") {
+            background = `linear-gradient(to top, var(--green-01), var(--green-02))`;
+        } else if (type === "enemy") {
+            background = `linear-gradient(to top, var(--red-01), var(--red-02))`;
+        } else if (type === "ban") {
+            background = `linear-gradient(to top, var(--neutral-02), var(--locked-gray-01))`;
+        }
+        return background;
+    };
+    return (
+        // main container
+        <div
+            className={`${styles.container} ${
+                type === "team"
+                    ? styles.team
+                    : type === "enemy"
+                    ? styles.enemy
+                    : ""
+            }`}
+        >
+            {/* shadow container */}
+            <div className={styles.outerShadowContainer}>
+                {/* image container */}
+                <div className={styles.imageContainer}>
+                    <div className={styles.imageWrapper}>
+                        <Image
+                            // src should be a prop
+                            src="/static/images/antimage.png"
+                            alt="where's the hero image bro?"
+                            width={80}
+                            height={65}
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* colored container */}
+            <div
+                className={styles.draftColorContainer}
+                style={
+                    type ? { backgroundImage: getTypeBackground(type) } : null
+                }
+            ></div>
         </div>
-      </div>
-      {/* colored container */}
-      <div
-        className={styles.draftColorContainer}
-        style={{
-          ...(type === "ban"
-            ? { backgroundColor: "var(--neutral-01)" }
-            : { backgroundImage: getTypeBackground(type) }),
-        }}
-      ></div>
-    </div>
-  )
-}
+    );
+};
 
-export default HeroDraftedContainer
+export default HeroDraftedContainer;
--- app/home/page.js
+++ app/home/page.js
@@ -1,14 +1,39 @@
 "use client";
 import React from "react";
+import HeroDraftedContainer from "../components/hero-drafted-container";
+import ImmortalLogo from "../components/immortal-logo";
+import styles from "./styles.module.css";
 
 export default function Home() {
     const videoData = "/static/videos/video-bg.webm";
 
+    const teamContainers = Array.from({ length: 5 }, (_, index) => (
+        <HeroDraftedContainer key={index} type={"team"} />
+    ));
+
+    const enemyContainers = Array.from({ length: 5 }, (_, index) => (
+        <HeroDraftedContainer key={index} type={"enemy"} />
+    ));
+
+    const banContainers = Array.from({ length: 11 }, (_, index) => (
+        <HeroDraftedContainer key={index} type={"ban"} />
+    ));
+
+
     return (
         <main className="main">
-            <video autoPlay muted loop>
+            <video autoPlay muted loop className={styles.videoBg}>
                 <source src={videoData} type="video/webm" />
             </video>
+
+            <header>
+                <div className={`${styles.header} flexRow`}>
+                    <div className="flexRow">{teamContainers}</div>
+                    <ImmortalLogo />
+                    <div className="flexRow">{enemyContainers}</div>
+                </div>
+                <div className={`${styles.header} flexRow`}>{banContainers}</div>
+            </header>
         </main>
     );
 }
